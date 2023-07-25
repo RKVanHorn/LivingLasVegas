d9f04b46cd39741e326f86cecb880918d853d595
RKVanHorn <acinonyx79@hotmail.com>
2023-07-25 15:34:00 -0700

    added search function to bird table

--- src/pages/Birding/BirdTable.jsx
+++ src/pages/Birding/BirdTable.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { Card, Table } from "react-bootstrap";
+import { Card, Table, Form } from "react-bootstrap";
 import Bird from "./Bird";
 
 export default function BirdTable({ birds, deleteBird, updateBird }) {
+  const [filtered, setFiltered] = React.useState("");
+
   return (
     <Card className="m-3 bg-primary text-white">
       <Card.Header className="text-center fs-4">
         Las Vegas Bird Sightings
       </Card.Header>
       <Card.Body className="table-responsive">
+        <Form>
+          <Form.Group className="mb-3" controlId="birdSearch">
+            <Form.Control
+              type="text"
+              placeholder="Search bird sightings"
+              value={filtered}
+              onChange={(e) => setFiltered(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -21,14 +33,29 @@ export default function BirdTable({ birds, deleteBird, updateBird }) {
             </tr>
           </thead>
           <tbody>
-            {birds.map((bird) => (
-              <Bird
-                key={bird.id}
-                bird={bird}
-                deleteBird={deleteBird}
-                updateBird={updateBird}
-              />
-            ))}
+            {birds
+              .filter((bird) => {
+                if (
+                  bird.birdName
+                    .toLowerCase()
+                    .includes(filtered.toLowerCase()) ||
+                  bird.location
+                    .toLowerCase()
+                    .includes(filtered.toLowerCase()) ||
+                  bird.notes.toLowerCase().includes(filtered.toLowerCase()) ||
+                  bird.date.toLowerCase().includes(filtered.toLowerCase())
+                ) {
+                  return bird;
+                }
+              })
+              .map((filteredBird) => (
+                <Bird
+                  key={filteredBird.id}
+                  bird={filteredBird}
+                  deleteBird={deleteBird}
+                  updateBird={updateBird}
+                />
+              ))}
           </tbody>
         </Table>
       </Card.Body>
