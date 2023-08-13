0e11cce687d8e12195cd6a88dd40c4ad163512a4
RKVanHorn <acinonyx79@hotmail.com>
2023-08-13 10:18:37 -0700

    added portfolio link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="pageFooter bg-primary">
-      Designed by RKVanHorn 2023
+      <a href="https://rvanhorn.dev" target="_blank">
+        Designed by RKVanHorn 2023
+      </a>
     </footer>
   );
 }
