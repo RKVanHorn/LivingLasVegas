59f29959bb7ec61a806dbebceffc38e95b441034
RKVanHorn <acinonyx79@hotmail.com>
2023-08-20 14:34:20 -0700

    trying to fix routing on live page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import "./App.css";
+
 import Home from "./pages/Home";
-import Layout from "./components/Layout";
+//import Layout from "./components/Layout";
+//import { Outlet } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Form from "react-bootstrap/Form";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
 import Birding from "./pages/Birding/Birding";
 import FAQs from "./pages/FAQs";
 import "bootswatch/dist/lux/bootstrap.min.css";
@@ -10,15 +18,35 @@ import "bootswatch/dist/lux/bootstrap.min.css";
 /**This is where I have my routes set up */
 
 function App() {
+  const [runConfetti, setRunConfetti] = React.useState(false);
+  const { width, height } = useWindowSize();
+  const toggleConfetti = () => {
+    setRunConfetti(!runConfetti);
+  };
+
   return (
     <BrowserRouter>
+      <Hero />
+      <Navigation />
+      <Form className="m-3">
+        <Form.Check // prettier-ignore
+          type="switch"
+          id="custom-switch"
+          label={runConfetti ? "Enough confetti!" : "Give me confetti!"}
+          onClick={toggleConfetti}
+        />
+      </Form>
+      {runConfetti && (
+        <Confetti numberOfPieces={700} width={width} height={height} />
+      )}
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="birding" element={<Birding />} />
-          <Route path="faqs" element={<FAQs />} />
-        </Route>
+        {/* <Route path="/" element={<Layout />}> */}
+        <Route index element={<Home />} />
+        <Route path="birding" element={<Birding />} />
+        <Route path="faqs" element={<FAQs />} />
+        {/* </Route> */}
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
